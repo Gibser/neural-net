3d4d6eb2289611d74fc5ab835c64477d07b440da
Gibser <rauso.giuseppe98@gmail.com>
2021-05-12 22:20:42 +0200

    Modifiche

--- neural_net/forward_step.m
+++ neural_net/forward_step.m
@@ -1,6 +1,6 @@
 function out = forward_step(net, x)
    z = x;
-   for i=1 : net.n_layers
+   for i=1 : net.n_layers-1
        a = net.weights{i} * z + net.biases{i};
        disp(a);
        z = net.activations{i}(a);
--- neural_net/net.m
+++ neural_net/net.m
@@ -1,20 +1,13 @@
-function net=net(dim, n_hidden, n_output, n_layers)
+function net=net(n_neurons, actv_functions, n_layers)
     SIGMA = 0.1;
     net.weights = {};
     net.biases = {};
     net.activations = {};
-    for i=1 : n_layers-1
-        net.weights{i} = SIGMA*randn(n_hidden, dim);
-        net.biases{i} = SIGMA*randn(n_hidden, 1);
-        net.activations{i} = @sigmoid;
+    for i=2 : n_layers
+        net.weights{i-1} = SIGMA*randn(n_neurons(i), n_neurons(i-1));
+        net.biases{i-1} = SIGMA*randn(n_neurons(i), 1);
+        net.activations{i-1} = actv_functions{i};
     end
-    %livello output
-    net.weights{end+1} = SIGMA*randn(n_output, n_hidden); 
-    net.biases{end+1} = SIGMA*randn(n_output, 1);
-    net.activations{end+1} = @identity;
-    net.n_hidden=n_hidden;
-    net.dim=dim;
-    net.n_output=n_output;
+    net.n_neurons=n_neurons;
     net.n_layers = n_layers;
-
 end
