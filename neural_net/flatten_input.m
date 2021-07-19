dee328ef52fd8980604e3ffd5d906456b3c87189
azankiew <checcol98@hotmail.it>
2021-07-19 15:40:22 +0200

    fix funzione flatten_input, aggiunta funzione flatten_kernel

--- neural_net/flatten_input.m
+++ neural_net/flatten_input.m
@@ -13,23 +13,19 @@ function [M] = flatten_input(input, k_h, k_w, s, P)
     H_out = (d(1) + 2*P - k_h)/s + 1;
     W_out = (d(2) + 2*P - k_w)/s + 1;
     M = zeros(d(3)*H_out*W_out, k_h*k_w*C_in);
-    
     for j=0 : d(3)*H_out*W_out-1
-        disp(j);
         l = floor(j / (H_out*W_out));
         p = mod(j, H_out*W_out);
         m = floor(p/H_out);
         t = mod(p, W_out);
         isw = t*s;
         ish = m*s;
-        disp(isw);
-        disp(ish);
         if length(d) == 4
             arr = input(ish+1:ish+k_h, isw+1:isw+k_w, l+1, :);
         else
             arr = input(ish+1:ish+k_h, isw+1:isw+k_w, l+1);
         end
-        M(j+1, :) = arr(:)';
+        M(j+1, :) = reshape(arr.',1,[]);
     end
 end
 
