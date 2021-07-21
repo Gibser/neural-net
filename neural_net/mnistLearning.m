d485eae06659220165b5b837c06f4e654d0a58e4
Gibser <rauso.giuseppe98@gmail.com>
2021-07-21 16:28:19 +0200

    Training funzionante

--- neural_net/mnistLearning.m
+++ neural_net/mnistLearning.m
@@ -1,10 +1,10 @@
-XTrain = X(:, :, 1:500);
+XTrain = X(:, :, 1:128);
 YTrain = Y(1:128);
 
-XVal = X(:, :, 501:1000);
+XVal = X(:, :, 129:256);
 YVal = Y(129:256);
 
 %load('layers2.mat');
 
 net = net_conv_FC(layers2, {@relu, @sigmoid}, {@reluDeriv, @sigmoidDeriv}, 3);
-[err, final_net, err_val] = learningPhase_convFC(net, 15, XTrain, XTrain, XVal, XVal, @MSE, @MSEDeriv, 0.0001, 1);
+[err, final_net, err_val] = learningPhase_convFC(net, 1000, XTrain, XTrain, XVal, XVal, @MSE, @MSEDeriv, 0.001, 1);
