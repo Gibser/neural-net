8043706f46f076cf12b96c3a7f27d1d199607e0f
unknown <mario.turco4@studenti.unina.it>
2021-07-21 23:21:37 +0200

    modifiche varie RPROP

--- RProp/sinLearning.m
+++ RProp/sinLearning.m
@@ -1,5 +1,5 @@
 eta=0.0001;
-MAX_EPOCHES=400;
+MAX_EPOCHES=5;
 
 
 X = randn(600, 1)';
@@ -27,6 +27,6 @@ tic
 n = net([1 3 1], {@sigmoid, @identity}, {@sigmoidDeriv, @identityDeriv}, 3);
 %disp([size(n.weightMomentums), size(n.weights)]);
 %[err, n, err_val] = learningPhase(n, MAX_EPOCHES, X, T, XVal, TVal, @sumOfSquares, @sumOfSquaresDeriv, eta, 1);
-[err, n, err_val] = learningPhase2(n, MAX_EPOCHES, X, T, XVal, TVal, @sumOfSquares, @sumOfSquaresDeriv, 1);
+[err, n, err_val] = learningPhase2(n, MAX_EPOCHES, X, T, XVal, TVal, @MSE, @MSEDeriv, 1);
 
 toc
