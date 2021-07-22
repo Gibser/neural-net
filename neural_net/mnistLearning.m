068ddc9eb3b5eebe351c05733968cf71ceb8ef71
Gibser <rauso.giuseppe98@gmail.com>
2021-07-22 15:31:10 +0200

    Aggiunta inizializzazione he_uniform per i pesi dei kernel

--- neural_net/mnistLearning.m
+++ neural_net/mnistLearning.m
@@ -1,10 +1,10 @@
-%XTrain = X(:, :, 1:1024);
-XTrain = X(:, :, 1);
+XTrain = X(:, :, 1:1024);
+%XTrain = X(:, :, 1);
 %YTrain = Y(1:128);
 %XTrain_01 = (XTrain - min(XTrain(:))) / (max(XTrain(:)) - min(XTrain(:)));
 
-%XVal = X(:, :, 1025:2048);
-XVal = X(:, :, 2);
+XVal = X(:, :, 1025:2048);
+%XVal = X(:, :, 2);
 %XVal_01 = (XVal - min(XVal)) / (max(XVal) - min(XVal));
 %YVal = Y(129:256);
 
