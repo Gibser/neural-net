e9476c0da991ca07b059bfaf27e61330a64c717a
Gibser <rauso.giuseppe98@gmail.com>
2021-07-22 16:14:53 +0200

    Aggiunta inizializzazione he_uniform per i pesi dei kernel

--- neural_net/mnistLearning.m
+++ neural_net/mnistLearning.m
@@ -11,4 +11,4 @@ XVal = X(:, :, 1025:2048);
 %load('layers2.mat');
 
 net = net_conv_FC(layers2, {@sigmoid, @sigmoid}, {@sigmoidDeriv, @sigmoidDeriv}, 3);
-[err, final_net, err_val] = learningPhase_convFC(net, 1000, XTrain, XTrain, XVal, XVal, @MSE, @MSEDeriv, 2, 0.01, 0.0, 128);
+[err, final_net, err_val] = learningPhase_convFC(net, 1000, XTrain, XTrain, XVal, XVal, @MSE, @MSEDeriv, 2, 0.01, 0.8, 128);
