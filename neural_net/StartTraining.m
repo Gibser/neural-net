3ad8217ae94cebd4826dd1600353632e56817a24
Gibser <rauso.giuseppe98@gmail.com>
2021-07-26 12:59:41 +0200

    fix online learning

--- neural_net/StartTraining.m
+++ neural_net/StartTraining.m
@@ -22,12 +22,12 @@ YT = build_Y(YT);
 YV = build_Y(YV);
 
 %% Load layers
-load('layers2.mat');            %cell array per i livelli
+load('layers.mat');            %cell array per i livelli
 load('actvFunc.mat');           %cell array per le funzioni di attivazione
 load('actvFuncDeriv.mat');      %cell array per le derivate delle funzioni di attivazione
 
 %% Training
 net = net_conv_FC(layers2, actvFunc, actvFuncDeriv, 3);
 tic
-[err, final_net, err_val, acc_tr, acc_val] = learningPhase_convFC(net, EPOCHE, XT, YT, XV, YV, @softMaxCrossEntropy, @softMaxCrossEntropyDeriv, 2, ETA, MOMENTUM, BATCH_SIZE);
+[err, final_net, err_val, acc_tr, acc_val] = learningPhase_convFC(net, EPOCHE, XT, YT, XV, YV, @softMaxCrossEntropy, @softMaxCrossEntropyDeriv, 0, ETA, MOMENTUM, BATCH_SIZE);
 toc
