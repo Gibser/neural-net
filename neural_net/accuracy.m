c2c9b7af13c24e72886bf45800a81b273929de79
Gibser <rauso.giuseppe98@gmail.com>
2021-07-26 15:33:49 +0200

    fix esperimenti

--- neural_net/accuracy.m
+++ neural_net/accuracy.m
@@ -4,9 +4,8 @@ function [acc] = accuracy(net, imgs, trueValues)
 % net - rete
 % imgs - dati da predirre
 % trueValue - etichette vere dei dati da predirre
-    %arr = softmax(Y);
     [~, z] = forward_step_convFC(net, imgs);
-    arr = softmax(z{end});
+    arr = softmax_function(z{end});
     [~, ind] = max(arr, [], 1);
     n_pred = sum((ind-1)==trueValues', 2);
     acc = (n_pred / length(trueValues))*100;
