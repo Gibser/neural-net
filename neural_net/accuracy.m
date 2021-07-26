51bfb2ab762454fc2c252348e583f8749737d782
Gibser <rauso.giuseppe98@gmail.com>
2021-07-26 13:57:46 +0200

    fix esperimenti

--- neural_net/accuracy.m
+++ neural_net/accuracy.m
@@ -9,16 +9,6 @@ function [acc] = accuracy(net, imgs, trueValues)
     arr = softmax(z{end});
     [~, ind] = max(arr, [], 1);
     n_pred = sum((ind-1)==trueValues', 2);
-    %{
-    for i=1 : length(trueValues)
-        arr = forward_step_convFC(net, imgs(:, :, i));
-        %disp(softmax(arr{end}));
-        %disp(T(i));
-        %[m, ind] = max(arr(:, i));
-        [m, ind] = max(softmax(arr{end}));
-        n_pred = n_pred + (ind-1==trueValues(i));
-    end
-    %}
     acc = (n_pred / length(trueValues))*100;
 end
 
