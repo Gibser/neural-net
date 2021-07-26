f46cab0a138798281b21255e21153ff60404b124
Gibser <rauso.giuseppe98@gmail.com>
2021-07-26 15:25:09 +0200

    fix esperimenti

--- neural_net/net_conv_FC.m
+++ neural_net/net_conv_FC.m
@@ -1,26 +1,12 @@
 function net = net_conv_FC(layers, actv_functions, deriv_func, n_layers)
-    %layers è un array di struct del tipo
-    %{
-        {
-          type:      0 per livello FC, 1 per livello conv
-          n_neurons: numero di neuroni se il livello è FC, numero di filtri
-                     se è conv
-          dim:     se type è conv, indica le dimensioni dei kernel
-          stride: indica lo stride per i kernel
-          padding: indica il padding da applicare all'input prima della convoluzione
-        }
-    %}
-    
     SIGMA = 0.1;
     net.weights = {};
-    %net.biases = {};
     net.activations = {};
     net.deriv_func = {};
     net.layers = layers;
     net.n_layers = n_layers;
     
     for i=2 : n_layers
-        
        if layers{i}.type == 2     
        %% Livello Convoluzionale
            if layers{i-1}.type ~= 0
@@ -68,24 +54,12 @@ function net = net_conv_FC(layers, actv_functions, deriv_func, n_layers)
                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 net.weights{i-1} = SIGMA*randn(layers{i}.n_neurons, layers{i-1}.n_neurons); 
            end
-           %disp(size(net.weights{i-1}));
-           %disp(net.layers{i-1}.output_shape);
            flatten = reshape(randn(net.layers{i-1}.output_shape), net.layers{i-1}.output_shape(1)*net.layers{i-1}.output_shape(2), []);
            net.layers{i}.output_shape = size(net.weights{i-1} * flatten);
            net.layers{i}.use_bias = 1;
            net.layers{i}.bias = SIGMA*randn(net.layers{i}.n_neurons, 1);
        end
-       %{
-       if net.layers{i}.type == 2
-            net.biases{i-1} = 0;%SIGMA*randn(net.layers{i}.output_shape(1), 1);%gpuArray(SIGMA*randn(n_neurons(i), 1));
-       else
-            net.biases{i-1} = SIGMA*randn(net.layers{i}.n_neurons, 1);
-       end
-       %}
        net.activations{i-1} = actv_functions{i-1};
        net.deriv_func{i-1} = deriv_func{i-1};
     end
-    
-    %net.activations{n_layers} = actv_functions{n_layers};
-    %net.deriv_func{n_layers} = deriv_func{n_layers};
 end
